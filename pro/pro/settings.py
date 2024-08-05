e8afd0168fea59f09ab1cd28bb48e427138ecadb
Suchkov Maksim <barbell.wire0b@icloud.com>
2024-08-05 14:34:06 +0300

    connected the 'jazzmin' library for the admin panel

--- pro/pro/settings.py
+++ pro/pro/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
