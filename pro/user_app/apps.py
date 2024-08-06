c2d12073026230c15a2d7312defba101ab7c362f
Suchkov Maksim <barbell.wire0b@icloud.com>
2024-08-06 13:46:59 +0300

    изменил отображение названия приложения "UserApp" на "Управление пользователями"

--- pro/user_app/apps.py
+++ pro/user_app/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class UserAppConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_app'
+    verbose_name = 'Управление пользователями'
