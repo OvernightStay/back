9ee44b230cdcf8c96b94b323815e6347fac5db8c
Suchkov Maksim <barbell.wire0b@icloud.com>
2024-08-06 14:02:31 +0300

    добавил в админку поле date_of_change (только для чтения)

--- pro/user_app/admin.py
+++ pro/user_app/admin.py
@@ -6,12 +6,14 @@ from .models import Player
 # Регистрируем модели в админке
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'training_check', 'login', 'email', 'phone', 'gender', 'date_joined')
+    list_display = (
+        'first_name', 'last_name', 'training_check', 'login', 'email', 'phone', 'gender', 'date_joined',
+        'date_of_change')
     search_fields = ('login', 'email', 'phone', 'last_name')
     list_filter = ('gender', 'is_staff', 'is_active')
     ordering = ('-date_joined',)
     list_display_links = ('first_name', 'last_name', 'login',)
-    readonly_fields = ('date_joined', 'training_check')
+    readonly_fields = ('date_joined', 'training_check', 'date_of_change')
     list_per_page = 10
     fieldsets = (
         (None, {
