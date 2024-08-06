4d2a93e0adfd59b674ee705f82227fdf2ce36456
Suchkov Maksim <barbell.wire0b@icloud.com>
2024-08-06 21:42:59 +0300

    разбил данные о пользователе на группы, добавил доп. поля в ридонли

--- pro/user_app/admin.py
+++ pro/user_app/admin.py
@@ -7,16 +7,23 @@ from .models import Player
 @admin.register(Player)
 class PlayerAdmin(admin.ModelAdmin):
     list_display = (
-        'first_name', 'last_name', 'training_check', 'login', 'email', 'phone', 'gender', 'date_joined',
+        'last_name', 'first_name', 'login', 'email', 'phone', 'gender', 'training_check', 'date_joined',
         'date_of_change')
-    search_fields = ('login', 'email', 'phone', 'last_name')
+    search_fields = ('login', 'email', 'phone', 'last_name', 'first_name',)
     list_filter = ('gender', 'is_staff', 'is_active')
     ordering = ('-date_joined',)
     list_display_links = ('first_name', 'last_name', 'login',)
-    readonly_fields = ('date_joined', 'training_check', 'date_of_change')
+    readonly_fields = ('date_joined', 'training_check', 'date_of_change', 'is_staff', 'is_active')
     list_per_page = 10
     fieldsets = (
-        (None, {
-            'fields': ('first_name', 'last_name', 'training_check', 'login', 'email', 'phone', 'gender', 'date_joined')
+        ("Личные данные", {
+            'fields': ('first_name', 'last_name', 'login', 'gender',)
+        }),
+        ("Контактная информация", {
+            'fields': ('email', 'phone',)
+        }),
+        ("Дополнительная информация", {
+            'description': "Указанная информация изменяется автоматически",
+            'fields': ('training_check', 'is_staff', 'is_active', 'date_joined', 'date_of_change',)
         }),
     )
