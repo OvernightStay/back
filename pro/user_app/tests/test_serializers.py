095f88ff8049b7f9338fde81e0c36d771b184059
Suchkov Maksim <barbell.wire0b@icloud.com>
2024-08-08 12:07:39 +0300

    добавлена тест для PlayerRegisterSerializer

--- pro/user_app/tests/test_serializers.py
+++ pro/user_app/tests/test_serializers.py
@@ -26,6 +26,37 @@ class SerializerTests(APITestCase):
         # Создание пользователя для тестирования сериализатора PlayerSerializer
         self.player = Player.objects.create_user(**self.user_data)
 
+    # Тест для PlayerRegisterSerializer
+    def test_player_register_serializer(self):
+        # Удаление существующего пользователя с таким же email
+        Player.objects.filter(email=self.user_data['email']).delete()
+
+        # Создание новых уникальных значений для полей, которые должны быть уникальными
+        unique_suffix = f'{self._testMethodName}_new'
+        new_user_data = {
+            'login': f'testuser_{unique_suffix}',
+            'email': f'testuser_{unique_suffix[:10]}@example.com',  # Ограничиваем длину email до 50 символов
+            'password': 'testpassword',
+            'phone': f'1234567890{unique_suffix[-1]}',  # Уникальный телефон, содержащий только цифры
+            'first_name': 'Test',
+            'last_name': 'User'
+        }
+        # Убедимся, что в поле phone передаются только цифры и их ровно 11
+        new_user_data['phone'] = ''.join(filter(str.isdigit, new_user_data['phone']))
+        if len(new_user_data['phone']) < 11:
+            new_user_data['phone'] += '0' * (11 - len(new_user_data['phone']))
+        elif len(new_user_data['phone']) > 11:
+            new_user_data['phone'] = new_user_data['phone'][:11]
+
+        serializer = PlayerRegisterSerializer(data=new_user_data)
+        self.assertTrue(serializer.is_valid(), msg=serializer.errors)  # Проверка валидности данных
+        player = serializer.save()
+        self.assertEqual(player.login, new_user_data['login'])  # Проверка логина
+        self.assertEqual(player.email, new_user_data['email'])  # Проверка email
+        self.assertEqual(player.phone, new_user_data['phone'])  # Проверка телефона
+        self.assertEqual(player.first_name, new_user_data['first_name'])  # Проверка имени
+        self.assertEqual(player.last_name, new_user_data['last_name'])  # Проверка фамилии
+
     # Тест для PlayerLoginSerializer
     def test_player_login_serializer(self):
         serializer = PlayerLoginSerializer(data=self.login_data)
