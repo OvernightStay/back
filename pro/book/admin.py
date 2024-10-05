4d143e4512f8f54ba55e97923f9ecd0baaa7ee28
Maksim Suchkov <96449353+Odiosus@users.noreply.github.com>
2024-10-05 14:38:42 +0300

    создал модели в приложении book. добавил в админку

--- pro/book/admin.py
+++ pro/book/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Book
 
-# Register your models here.
+# Регистрация модели Book в админ-панели
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'date_add', 'date_update')  # Поля, которые будут отображаться в списке объектов
+    list_filter = ('date_add', 'date_update')  # Фильтры в правой боковой панели
+    search_fields = ('title', 'text')  # Поля, по которым можно осуществлять поиск
+    prepopulated_fields = {'slug': ('title',)}  # Автоматическое заполнение поля slug на основе поля title
+    readonly_fields = ('date_add', 'date_update')  # Поля, которые будут доступны только для чтения
+
+    fieldsets = (
+        (None, {
+            'fields': ('title', 'text', 'slug', 'image_start', 'image_end')
+        }),
+        ('Даты', {
+            'fields': ('date_add', 'date_update'),
+            'classes': ('collapse',),
+        }),
+    )
--- pro/book/models.py
+++ pro/book/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100, verbose_name='Глава')
+    text = models.TextField(verbose_name='Текст')
+    slug = models.SlugField(max_length=255, unique=True, db_index=True, verbose_name="URL",
+                            help_text="это поле заполняется автоматически")
+    image_start = models.ImageField(upload_to='image_start/', verbose_name='Изображение в начале', null=True,
+                                    blank=True)
+    image_end = models.ImageField(upload_to='image_end/', verbose_name='Изображение в конце', null=True, blank=True)
+    date_add = models.DateTimeField(auto_now_add=True, verbose_name='Дата создания')
+    date_update = models.DateTimeField(auto_now=True, verbose_name='Дата изменения')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Книга знаний'
+        verbose_name_plural = 'Книга знаний'
