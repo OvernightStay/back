d43ca2edf40c76c267d32ee1dee49a0a39363d51
Maksim Suchkov <96449353+Odiosus@users.noreply.github.com>
2024-10-05 14:57:14 +0300

    добавил вьюхи, урлы, сериализатор

--- pro/pro/urls.py
+++ pro/pro/urls.py
@@ -23,6 +23,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('user_app.urls')),
     path('api/', include('mini_location_app.urls')),
+    path('book/', include('book.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
