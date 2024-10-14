be48af2eeb780cf86171228b7f41a7520a9a9bf0
Maksim Suchkov <96449353+Odiosus@users.noreply.github.com>
2024-10-14 17:18:16 +0300

    настройка хранения медиафайлов

--- pro/pro/settings.py
+++ pro/pro/settings.py
@@ -180,6 +180,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
